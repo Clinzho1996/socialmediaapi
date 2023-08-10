d36311d2c572f950ea20d41da33c924e1665b58b
Clinzho1996 <confidinho@yahoo.com>
2023-08-10 08:20:56 +0100

    port updated

--- api/index.js
+++ api/index.js
@@ -19,7 +19,7 @@ mongoose
     useUnifiedTopology: true,
   })
   .then(() => {
-    app.listen(3000, () => {
+    app.listen(process.env.PORT || 3000, () => {
       console.log("App is running on http://localhost:3000");
     });
   })
